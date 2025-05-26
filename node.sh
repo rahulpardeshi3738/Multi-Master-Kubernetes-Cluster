d1a3c6b24c586311eb8d0822b2748146b49571dc
rahulpardeshi3738 <rahul.pardeshi3738@gmail.com>
2025-05-26 10:11:56 +0530

    script to initialization worker node

---
+++ node.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Disable swap
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Update system packages
+sudo apt update && sudo apt upgrade -y
+
+
+# Add kernel settings & Enable IP tables (CNI Prerequisites)
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl --system
+
+# Install containerd run time
+
+apt-get update -y 
+apt-get install ca-certificates curl gnupg lsb-release -y
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get install containerd.io -y
+
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd
+
+# ctr version
+
+# Install Kubernetes Tools (Kubeadm, Kubectl, kubelet)
+
+apt-get update -y
+apt-get install -y apt-transport-https ca-certificates curl gpg
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update -y
+apt-get install -y kubelet kubeadm
+apt-mark hold kubelet kubeadm
+
+systemctl daemon-reload 
+systemctl start kubelet 
+systemctl enable kubelet
